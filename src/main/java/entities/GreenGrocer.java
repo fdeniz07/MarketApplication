106c8296fee0634622e05eca21150c4652fb89c6
Fatih Deniz <deniz.fatih@hotmail.de>
2023-01-05 18:22:00 +0100

    Initial commit

---
+++ src/main/java/entities/GreenGrocer.java
@@ -0,0 +1,44 @@
+package entities;
+
+
+/*
+   No   Product    Price
+    ===== ======= ========
+    00 Tomato 2.10 TL ,
+    01 Potato 3.20 TL ,
+    02 Pepper 1.50 TL ,
+    03 Onion 2.30 TL  ,
+    04 Carrot 3.10 TL
+    05 Apple 1.20 TL
+    06 Banana 1.90 TL
+    07 Strawberry 6.10 TL
+    08 Melon 4.30 TL
+    09 Grape 2.70 TL
+    10 Lemon 0.50 TL
+ */
+
+import java.util.Date;
+
+public class GreenGrocer {
+
+    //  int productId;
+    String product;
+
+    double price;
+
+    double lastPrice;
+
+    double unitIsStock;
+
+    Date mfgDate; // Manufacturing Date --> Imalat tarihi
+
+   // Date arrivalDate; //Ürün gelis tarihi
+    Date expDate; //Expired Date --> son kullanim tarihi --> long
+
+
+
+}
+
+
+
+
